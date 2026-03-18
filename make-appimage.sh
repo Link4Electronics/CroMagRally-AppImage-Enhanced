b47c68d572284fe1ece212a2715692192be354d5
Link4Electronics <link4electronics@gmail.com>
2026-03-17 22:32:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q cro-mag-rally | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q cro-mag-rally | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/cro-mag-rally.png
 export DESKTOP=/usr/share/applications/cro-mag-rally.desktop
@@ -18,7 +18,9 @@ quick-sharun /usr/lib/cro-mag-rally/CroMagRally
 cp -r /usr/lib/cro-mag-rally/Data ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
